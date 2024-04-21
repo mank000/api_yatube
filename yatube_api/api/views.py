fb4362532cb8594be5de6d01b6c6db61f59ad614
Козьмин Артём <kozmin_arti@mail.ru>
2024-04-21 21:05:30 +0700

    first

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -28,14 +28,18 @@ class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = [IsAuthenticated, AuthorOfPost]
 
+    @staticmethod
+    def get_post(post_id):
+        return get_object_or_404(Post, pk=post_id)
+
     def get_queryset(self):
-        post = get_object_or_404(Post, id=self.kwargs.get('post_id'))
+        post = self.get_post(self.kwargs.get('post_id'))
         return post.comments.all()
 
     def perform_create(self, serializer):
         serializer.save(
             author=self.request.user,
-            post=get_object_or_404(Post, id=self.kwargs.get('post_id'))
+            post=self.get_post(self.kwargs.get('post_id'))
         )
 
 
